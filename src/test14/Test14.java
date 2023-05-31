2aeab6d9eee8a27f8ca460e143727cb0f0cafc80
504BassSlapper <504bassslapper@gmail.com>
2023-05-31 19:10:52 +0200

    test 14 solution

--- src/test14/Test14.java
+++ src/test14/Test14.java
@@ -1,28 +1,45 @@
 package test14;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class Test14 {
     // if you want it to compile
-//    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        try (var is = new ObjectInputStream(
+                new BufferedInputStream(
+                        new FileInputStream("file")))) {
+            Bird b = (Bird) is.readObject();
+        }
+    }
+//    public static void main(String[] args) {
 //        try(var is = new ObjectInputStream(
 //                new BufferedInputStream(
 //                        new FileInputStream("file")))){
-//            Bird b = (Bird) is.readObject();
+//            Bird b =  is.readObject();
 //        }
 //    }
-    public static void main(String[] args) {
-        try(var is = new ObjectInputStream(
-                new BufferedInputStream(
-                        new FileInputStream("file")))){
-            Bird b =  is.readObject();
-        }
-    }
 
     private static class Bird {
         private String name;
         private int age;
-        private
 
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
     }
 }
