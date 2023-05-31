80d004afa66acf49bc6e30c4c0b6a6470933db87
504BassSlapper <504bassslapper@gmail.com>
2023-05-31 18:45:03 +0200

    realisation du test 12

--- src/test12/Test12.java
+++ src/test12/Test12.java
@@ -1,4 +1,12 @@
 package test12;
 
+import java.sql.*;
+
 public class Test12 {
+    public static void main(String[] args) throws SQLException {
+        Connection a = DriverManager.getConnection("url", "user", "pass");
+        PreparedStatement b = a.prepareStatement("sql statement");
+        ResultSet c = b.executeQuery();
+
+    }
 }
