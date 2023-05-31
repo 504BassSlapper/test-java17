ebc1e5a430bb68bd7a687b6dc05c8a2e1827255f
504BassSlapper <504bassslapper@gmail.com>
2023-05-31 19:18:18 +0200

    test 15 solution

---
+++ src/test15/Test15.java
@@ -0,0 +1,28 @@
+package test15;
+
+public class Test15 {
+    public static void main(String[] args) {
+        var var = 3;
+        // not valid instance member of a class
+        // case is reserved
+        // Var case = new Var();
+// new is reserved
+//        String new = "var";
+    }
+
+    void var() {
+
+    }
+
+
+//    var var() {
+//        return null;
+//    }
+
+    // neither
+//    int Var(){
+//        var _ = 7;
+//        return _;
+//
+//    }
+}
